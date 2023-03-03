2aadcc6c2c62a0b66084f5aa81ef6c72c0a95776
Pedro Evers <pedroevers@gmail.com>
2023-03-02 22:22:01 -0300

    Continuidade

---
+++ ProjetosIntegrados/ProjetoNotas/src/ProjetoNotas.Application.API/Controllers/NoteController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoNotas.Domain.Interfaces.IService;
+
+namespace ProjetoNotas.Application.API.Controllers
+{
+    public class NoteController : Controller
+    {
+        private readonly INoteService _noteService;
+        public NoteController(INoteService noteService)
+        {
+            _noteService = noteService;
+        }
+        public IActionResult Index()
+        {
+            var listNoteUser = _noteService.FindAll();
+            return View();
+        }
+    }
+}
---
+++ ProjetosIntegrados/ProjetoNotas/src/ProjetoNotas.Application.API/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ProjetoNotas.Domain.DTO;
+using ProjetoNotas.Domain.Interfaces.IService;
+
+namespace ProjetoNotas.Application.API.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService service)
+        {
+            _userService = service;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var list = _userService.FindAll();
+            return View(list);
+        }
+        public JsonResult ListJson()
+        {
+            return Json(_userService.FindAll());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("id, login, senha, email")] UserDTO user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (await _userService.Save(user) > 0)
+                    return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+    }
+}
